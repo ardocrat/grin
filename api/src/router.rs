3109dfd3debb09764246bb0c3b74c6bdf9d19fb3
ardocrat <ardocrat@gri.mw>
2026-07-07 16:04:44 +0300

    Fix tests using rustls (#3885)
    * fix: init default rustls provider
    
    * fix: do not crash on provider install

--- api/src/router.rs
+++ api/src/router.rs
@@ -347,6 +347,7 @@ mod tests {
 
 	#[test]
 	fn test_get() {
+		let _ = rustls::crypto::ring::default_provider().install_default();
 		util::init_test_logger();
 		let mut routes = Router::new();
 		routes
--- api/tests/rest.rs
+++ api/tests/rest.rs
@@ -73,6 +73,7 @@ fn open_port(host: &str) -> u16 {
 
 #[test]
 fn test_start_api() {
+	let _ = rustls::crypto::ring::default_provider().install_default();
 	util::init_test_logger();
 	let mut server = ApiServer::new();
 	let mut router = build_router();
@@ -101,6 +102,7 @@ fn test_start_api() {
 #[ignore]
 #[test]
 fn test_start_api_tls() {
+	let _ = rustls::crypto::ring::default_provider().install_default();
 	util::init_test_logger();
 	let tls_conf = TLSConfig::new(
 		"tests/fullchain.pem".to_string(),
